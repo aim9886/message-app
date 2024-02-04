4cb7e9c4903e484310edcae92216ecfa7770c0db
aim9886 <aimanth9886@gmail.com>
2024-02-04 17:48:31 +0530

    ninth commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client"
 import './index.css';
-import App from "./message-app-client/App.jsx";
+import App from "./Razorpay/App.jsx";
 // import global from "./global.css"
 
 createRoot(document.getElementById("root")).render(<App/>)
